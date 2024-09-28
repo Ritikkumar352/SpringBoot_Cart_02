5bb440c07d0aa7fb22181a68e8fbdf1d944a7da9
ritikkumar352 <krritik2002@gmail.com>
2024-09-29 00:18:32 +0530

    cross origin ..App is Up and running

--- ecom_1/src/main/java/com/ritik/ecom_1/controller/productController.java
+++ ecom_1/src/main/java/com/ritik/ecom_1/controller/productController.java
@@ -4,6 +4,7 @@ package com.ritik.ecom_1.controller;
 import com.ritik.ecom_1.model.Product;
 import com.ritik.ecom_1.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class productController {
     @Autowired
--- ecom_1/src/main/resources/data.sql
+++ ecom_1/src/main/resources/data.sql
@@ -4,4 +4,5 @@ VALUES
 (2, 'Smartphone', 'Latest smartphone', 'BrandY', 30000, 'Electronics', '2023-03-22', true, 25),
 (3, 'Headphones', 'Wireless headphones', 'BrandZ', 2000, 'Accessories', '2023-02-10', true, 50),
 (4, 'Smartwatch', 'Smartwatch with fitness tracker', 'BrandA', 15000, 'Wearables', '2023-04-01', true, 15),
-(5, 'Camera', 'DSLR camera with lens', 'BrandB', 60000, 'Photography', '2023-05-05', true, 5);
+(5, 'Camera', 'DSLR camera with lens', 'BrandB', 60000, 'Photography', '2023-05-05', true, 5),
+(6, 'Smartphone', 'Powerful Smartphone with AI and bigger battery, and AmOled Display ', 'Samasung', 60000, 'Electronics', '2023-05-05', true, 5);
